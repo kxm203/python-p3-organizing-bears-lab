06f5f9897da33916af25aea11384482b09ae832a
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-03-04 11:15:33 -0500

    AAAAHHH bearsgit add .git add .

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocoloate", 32, "M", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 25, "M", "Black", "Aggressive", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 20, "F", "Brown", "Calm", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 35, "M", "Black", "Agressive", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "F", "Brown", "Agressvive", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 42, "M", "Black", "Agressive", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 28, "F", "Brown", "Calm", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, "M", "Black", "Agressive", True);
